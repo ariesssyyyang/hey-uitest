953389922aedb05d85e73ea25a44c3ae85b1fb3a
Aries Yang <ariesssyyyang@gmail.com>
2021-06-04 22:51:56 +0800

    initial commit

---
+++ ContactBook/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ContactBook
+//
+//  Created by Aries Yang on 2021/6/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
